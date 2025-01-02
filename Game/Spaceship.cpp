2a317f205018f001ef4e6a318d622ad8757cd36c
wiresnchains <dgrachov@icloud.com>
2025-01-02 17:09:34 +0100

    Remove old commented code

--- Game/Spaceship.cpp
+++ Game/Spaceship.cpp
@@ -9,37 +9,3 @@ using namespace Spacerush::Game;
 void CSpaceship::Shoot(MoveDirection direction, ProjectileTargetType targetType) {
 	State::Projectiles.push_back(new CProjectile(State::Projectiles.size(), Position, direction, targetType));
 }
-
-/*void CSpaceship::ProcessBullets() {
-	for (int i = 0; i < Bullets.size(); i++) {
-		CBullet* bullet = Bullets[i];
-
-		bullet->Draw();
-		bullet->ProcessMovement();
-
-		switch (TargetEntites) {
-		case PROJECTILE_TARGET_PLAYER:
-			if (bullet->CollidesWith(State::Player)) {
-				State::GameOver = true;
-			}
-
-			break;
-		case PROJECTILE_TARGET_ENEMIES:
-			for (int j = 0; j < State::Enemies.size(); j++) {
-				CEnemy* enemy = State::Enemies[j];
-
-				if (enemy == this)
-					continue;
-
-				if (bullet->CollidesWith(enemy)) {
-					State::Enemies.erase(State::Enemies.begin() + j);
-					Bullets.erase(Bullets.begin() + i);
-
-					State::Player->Score++;
-				}
-			}
-
-			break;
-		}
-	}
-}*/
